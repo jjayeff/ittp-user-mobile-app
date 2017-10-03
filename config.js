d02c955a8dd10fc41dcd39cba2e135a9976ba42a
Admin <ekkawit.lertkrai@gmail.com>
2017-10-03 15:26:06 +0700

    connect real API from ITTP

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// export const API_SERVER = 'http://174.138.31.29:3000';
-// export const CID = '3300900307881';
+export const API_SERVER = 'http://174.138.31.29:3000';
+export const CID = '3300900019036';
 
-export const API_SERVER = 'http://localhost:3000';
-export const CID = '4101200009225';
+// export const API_SERVER = 'http://localhost:3000';
+// export const CID = '4101200009225';
 
-export const mockup = false;
+export const mockup = true;
